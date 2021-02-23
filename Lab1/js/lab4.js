682635868cfee6a2cdb7d72cf072c72db4b1c52f
LBanda <A01610329@itesm.mx>
2021-02-22 21:02:07 -0600

    inicio lap4, falta terminar

---
+++ Lab1/js/lab4.js
@@ -0,0 +1,5 @@
+console.log('Ejercicios Lab4');
+
+console.info('1:Entrada: un número pedido con un prompt. Salida: Una tabla con los números del 1 al número dado con sus cuadrados y cubos. Utiliza document.write para producir la salida');
+
+const numero = window.prompt('Escribe un numero entero y positivo');
