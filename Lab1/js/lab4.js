1089b2d2662b06bb38d32ecca041073aaa3b590c
LBanda <A01610329@itesm.mx>
2021-03-04 16:24:07 -0600

    cambios laboratorios

--- Lab1/js/lab4.js
+++ Lab1/js/lab4.js
@@ -3,3 +3,37 @@ console.log('Ejercicios Lab4');
 console.info('1:Entrada: un número pedido con un prompt. Salida: Una tabla con los números del 1 al número dado con sus cuadrados y cubos. Utiliza document.write para producir la salida');
 
 const numero = window.prompt('Escribe un numero entero y positivo');
+
+document.write('1:Entrada: un número pedido con un prompt. Salida: Una tabla con los números del 1 al número dado con sus cuadrados y cubos. Utiliza document.write para producir la salida <br>');
+
+document.write("<table>");
+    for(let i = 1; i <= numero; i++){
+        document.write("<tr>"+"<td>"+ i + "</td>"+"<td>"+ i*i + "</td>"+"<td>"+ i*i*i + "</td>"+"</tr>");
+    }
+document.write("</table>");
+
+ 
+
+console.info('2:Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta.');
+
+let S = new Date().getSeconds();
+let x = Math.round(Math.random()*10);
+let y = Math.round(Math.random()*10);
+
+let suma = x + y;
+
+const resultado = window.prompt('Escribe el resultado de la suma de ' + x + ' + ' + y);
+
+document.write('2:Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta. <br>');
+
+    if(resultado == suma){
+        document.write("Correcto  Tiempo: " + S + " segundos <br>");
+    }
+    
+    else{
+        document.write("Incorrecto  Tiempo: " + S + " segundos <br>");
+    }
+
+document.write('3:Función: contador. Parámetros: Un arreglo de números. Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0s, y la cantidad de valores mayores a 0 en el arreglo.<br>');
+
+
